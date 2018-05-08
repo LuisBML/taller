e0b92834e1cd74d3f4ab01c80f205a3bd45376c1
LuisBML <bryan_mtz95@hotmail.com>
2018-05-08 00:23:43 -0600

    En blanco

---
+++ Turismo/app/src/main/java/turismo/taller/itchii/turismo/MainActivity.java
@@ -0,0 +1,13 @@
+package turismo.taller.itchii.turismo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
