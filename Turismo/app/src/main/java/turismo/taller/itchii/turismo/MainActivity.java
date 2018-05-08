f430ee9331ed653ca81598d9ecb9c979867d1f03
LuisBML <bryan_mtz95@hotmail.com>
2018-05-08 11:57:22 -0600

    Navigation drawer. Falta definir secciones y sus fragments

--- Turismo/app/src/main/java/turismo/taller/itchii/turismo/MainActivity.java
+++ Turismo/app/src/main/java/turismo/taller/itchii/turismo/MainActivity.java
@@ -1,13 +1,49 @@
 package turismo.taller.itchii.turismo;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navView);
+    }
+
+    private void setToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Establece la barra de herramientas como la barra de app de la actividad
+        setSupportActionBar(toolbar);
+
+        //Establecer icono para el menu
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //Abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
